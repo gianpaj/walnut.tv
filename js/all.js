8a8e766a9b7f70fece2c07656b9d76c053e81efe
Gianfranco Palumbo <gianpa@gmail.com>
2021-08-10 09:06:51 +0100

    minor

--- js/all.js
+++ js/all.js
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-console
-console.log('%c%s', 'color: green; background: yellow; font-size: 24px;', 'Thanks for checking walnut source code!');
+console.log('%c%s', 'color: green; background: yellow; font-size: 24px;', "Thanks for checking walnut source's code!");
 // eslint-disable-next-line no-console
 console.log("Walnut's code is open sourced at https://github.com/gianpaj/walnut.tv");
 
@@ -17,9 +17,8 @@ const channels = [
     youtubeChannels:
       'UCX6b17PVsYBQ0ip5gyeme-Q;UCmmPgObSUPw1HL2lq6H4ffA;UC7IcJI8PUf5Z3zKxnZvTBog;UCZYTClx2T1of7BRZ86-8fow;UC9uD-W5zQHQuAVT2GdcLCvg;UCsXVk37bltHxD1rDPwtNM8Q;UC6107grRI4m0o2-emgoDnAA;UCHnyfMqiRRG1u-2MsSQLbXA;UC6nSFpj9HTCZ5t-N3Rm3-HA;UC7DdEm33SyaTDtWYGO2CwdA;UC3KEoMzNz8eYnwBC34RaKCQ;UCt_t6FwNsqr3WWoL6dFqG9w;UClIZqOLqUCro7bKztUjYCNA;UCC552Sd-3nyi_tk2BudLUzA;UCYLrBefhyp8YyI9VGPbghvw;UCH4BNI0-FOK2dMXoFtViWHw;UCeiYXex_fwgYDonaTcSIk6w;UC7_gcs09iThXybpVgjHZ_7g;UCpJmBQ8iNHXeQ7jQWDyGe3A;UCfMJ2MchTSW2kWaT0kK94Yw;UCtHaxi4GTYDpJgMSGy7AeSw;UCHsRtomD4twRf5WVHHk-cMw;UCbwp5B-uDBy-fS4bDA0TEaw;UCj1VqrHhDte54oLgPG4xpuQ;UCIRiWCPZoUyZDbydIqitHtQ;UCY1kMZp36IQSyNx_9h4mpCg;UC9RM-iSvTu1uPJb8X5yp3EQ;UCEik-U3T6u6JA0XiHLbNbOw;UCYO_jab_esuFRV4b17AJtAw;UCR1IuLEqb6UEA_zQ81kwXfg;UCtxJFU9DgUhfr2J2bveCHkQ;UCEIwxahdLz7bap-VDs9h35A;UCBa659QWEk1AI4Tg--mrJ2A;UCP5tjEmvPItGyLhmjdwP7Ww;UCUHW94eEFW7hkUMVaZz4eDg;UCzWQYUVCpZqtN93H8RR44Qw',
   },
- 
-  
-   {
+
+  {
     title: 'science',
     subreddit: 'biology;psychology;lectures;space;philosophy;physics;math',
     minNumOfVotes: 3,
