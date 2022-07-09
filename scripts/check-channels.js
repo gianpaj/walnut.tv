cad71d094e56dea6389d616719b9f0487c267107
Gianfranco Palumbo <gianpa@gmail.com>
2022-07-09 12:42:39 +0100

    improve check-channels script error output by using yup validator

--- scripts/check-channels.js
+++ scripts/check-channels.js
@@ -1,28 +1,39 @@
 // this script checks if the Channels Ids are in a correct format
+const { object, string, array } = require('yup');
 
 const { channels } = require('../channels');
 
+const validateYoutubeChannelIds = (channelIds) => {
+  const schema = object().shape({
+    channelIds: array()
+      .transform(function (value, originalValue) {
+        if (this.isType(value) && value !== null) {
+          return value;
+        }
+        return originalValue ? originalValue.split(/[\s;]+/) : [];
+      })
+      .of(string().length(24).required()),
+  });
+
+  return schema.validateSync({ channelIds });
+};
+
 const errors = channels
   .map((channel) => {
-    if (channel.youtubeChannels) {
-      const channelIds = channel.youtubeChannels.split(';');
-      const cc = channelIds
-        .map((channelId) => {
-          if (channelId.length !== 24) {
-            return new Error();
-          }
-          return channelId;
-        })
-        .filter((c) => c instanceof Error);
-      if (cc.length > 0) {
-        const error = new Error(
-          `The "${channel.title}" Walnut channel has an incorrect YouTube Channels Id(s):\n${channelIds}\n\n`
-        );
-        console.log(error.message);
-        return error;
-      }
+    if (!channel.youtubeChannels) {
+      return null;
     }
-    return channel;
+    try {
+      validateYoutubeChannelIds(channel.youtubeChannels);
+      return channel;
+    } catch (error) {
+      console.log(`The "${channel.title}" Walnut channel has an incorrect YouTube Channels Id(s):`);
+      console.log({
+        message: error.errors,
+        params: error.params,
+      });
+      return error;
+    }
   })
   .filter((c) => c instanceof Error);
 
