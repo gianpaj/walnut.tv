956097853b0d97197347afce046b54df381f9f04
Gianfranco Palumbo <gianpa@gmail.com>
2022-10-03 10:55:13 +0100

    fix eslint config

--- js/all.js
+++ js/all.js
@@ -68,18 +68,17 @@ function RedditVideoService() {
     return result;
   }
 
-  // eslint-disable-next-line no-unused-vars
-  function dynamicSort(property) {
-    let sortOrder = 1;
-    if (property[0] === '-') {
-      sortOrder = -1;
-      property = property.substr(1);
-    }
-    return function (a, b) {
-      const result = a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
-      return result * sortOrder;
-    };
-  }
+  // function dynamicSort(property) {
+  //   let sortOrder = 1;
+  //   if (property[0] === '-') {
+  //     sortOrder = -1;
+  //     property = property.substr(1);
+  //   }
+  //   return function (a, b) {
+  //     const result = a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+  //     return result * sortOrder;
+  //   };
+  // }
 
   function _loadHot(channel, upsMin, after) {
     return new Promise((result, reject) => {
